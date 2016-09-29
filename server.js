22e3fda2f97296691bd19bbc51690f3838f1f405
aadithpm <aadithpmdesigns@gmail.com>
2016-09-29 19:10:49 +0530

    Modified port number

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+var port = 9013; // Use 8080 for local development because you might already have apache running on 80
+app.listen(9013, function () {
+  console.log(`NodeJS Server listening on port:${port}!`);
 });
